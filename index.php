86f68760cfb05d2d866337433cdf3a43fbe75acb
Illensky <alexis.laroche.02240@gmail.com>
2022-01-06 15:30:51 +0100

    Exercice done

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+$texte = "Du texte est stocké";
+echo "<pre>";
+echo $texte;
+echo "</pre>";
+
+
+$y = 30;
+echo "<pre>";
+echo "y vaux $y";
+echo "</pre>";
+
+$m = 5;
+$n = 7;
+echo "<pre>";
+echo $m + $n;
+echo "</pre>";
+
+?>
